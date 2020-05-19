3cbbfd0cbe41559d4c032b300c215f50c87b1124
mako4kamiya <mako4kamiya@gmail.com>
2020-05-19 15:53:59 +0900

    500の例外の発生と処理を記載

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,17 @@
 class ApplicationController < ActionController::Base
     layout :set_layout
 
+    rescue_from StandardError, with: :rescue500 #例外が発生したら、メソッドrescue500を処理する
+
     private def set_layout
         if params[:controller].match(%r{\A(staff|admin|customer)/})
             Regexp.last_match[1]
         else
             "customer"
         end
+    end
+
+    private def rescue500(e) #eにはExceptionオブジェクトが渡される。
+        render "errors/internal_server_error", status: 500
     end
 end
--- app/controllers/staff/top_controller.rb
+++ app/controllers/staff/top_controller.rb
@@ -1,5 +1,6 @@
 class Staff::TopController < ApplicationController
     def index
+        raise
         render action: "index" #(省略可能)
     end
 end
